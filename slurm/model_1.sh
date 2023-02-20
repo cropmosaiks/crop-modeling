4e6c348d88baa5ccde54a01578134437e1ed5c31
cullen-molitor <cullen.molitor@gmail.com>
2023-02-20 13:17:22 -0800

    time

--- slurm/model_1.sh
+++ slurm/model_1.sh
@@ -7,4 +7,4 @@ cd $SLURM_SUBMIT_DIR
 /bin/hostname
 source /home/cmolitor/.bashrc
 conda activate mosaiks-env
-time mpirun -np $SLURM_NTASKS  /home/cmolitor/crop_modeling/code/3_task_modeling/model_1_sensor.py >& logfile
+mpirun -np $SLURM_NTASKS /home/cmolitor/crop_modeling/code/3_task_modeling/model_1_sensor.py >& logfile
