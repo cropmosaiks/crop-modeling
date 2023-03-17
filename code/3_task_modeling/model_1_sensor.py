3991c28da5ab4489246925e8f08681df1a28acf9
cullen-molitor <cullen.molitor@gmail.com>
2023-03-17 09:05:57 -0700

    starmap

--- code/3_task_modeling/model_1_sensor.py
+++ code/3_task_modeling/model_1_sensor.py
@@ -9,17 +9,21 @@ from mpi4py.futures import MPIPoolExecutor
 directory = here("data", "random_features", "summary")
 files = os.listdir(directory)
 files = [f for f in files if f not in ('.gitkeep', '.ipynb_checkpoints')]
-paramlist = list(itertools.product(files, [True, False]))
+paramlist = itertools.product(files, [True, False])
 paramlist = sorted(paramlist, key=lambda tup: tup[1])
-paramlist = paramlist[0:5]
+
+# paramlist = paramlist[0:44]
+
+paramlist = (i for i in paramlist)
 
 if __name__ == "__main__":
-    output = []
-    executor = MPIPoolExecutor()
-    for result in executor.map(model_1_sensor, paramlist):
-        output.append(result)
+    max_workers = int(os.environ.get("SLURM_NTASKS", 4)) - 1
+
+    executor = MPIPoolExecutor(max_workers=max_workers)
+    output = executor.starmap(model_1_sensor, paramlist)
     executor.shutdown()
-    results = pd.concat(output).reset_index(drop=True)
+
+    results = pd.DataFrame(output)
     today = date.today().strftime("%Y-%m-%d")
     file_name = f'results_{today}.csv'
     print(f"Saving results as: {file_name}\n\n")
--- slurm/model_1.sh
+++ slurm/model_1.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -l
-
-# Just get 89 cores, don't care on what nodes
-#SBATCH --ntasks=89         
+#SBATCH --ntasks=50           # Do n many tasks at once
+#SBATCH --cpus-per-task=4     # Give each task n cpus             
 #SBATCH --output slurm/%j.out # File to save job's STDOUT (%j = JobId)
 #SBATCH --error slurm/%j.err  # File to save job's STDERR
-
-# Send an e-mail when a job starts, stops, or fails
 #SBATCH --mail-user=cullen_molitor@ucsb.edu
-#SBATCH --mail-type=ALL 
+#SBATCH --mail-type=ALL   # Send an e-mail when a job starts, stops, or fails
 
 module purge
 module load openmpi/3.1.3
 
 export OMPI_MCA_mpi_warn_on_fork=0
 
-source /home/cmolitor/.bashrc
+source /home/cmolitor/anaconda3/etc/profile.d/conda.sh
 conda activate mosaiks-env
 
 cd $SLURM_SUBMIT_DIR
