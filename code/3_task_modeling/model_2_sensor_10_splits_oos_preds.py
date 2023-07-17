97a88f39d12d1d766a5dc9200eafa3ce17f06253
cullen-molitor <cullen.molitor@gmail.com>
2023-07-17 13:57:34 -0700

    more cleanup

--- code/3_task_modeling/model_2_sensor_10_splits_oos_preds.py
+++ code/3_task_modeling/model_2_sensor_10_splits_oos_preds.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     random.seed(42)
     random_seeds = [random.randint(0, 1_000_000) for _ in range(n_splits)]
 
-    f1 = "landsat-c2-l2_bands-r-g-b-nir-swir16-swir22_ZMB_20k-points_1024-features_yr-2009-2021_mn-4-9_lm-True_cm-True_wa-False_summary.feather"
-    f2 = "sentinel-2-l2a_bands-2-3-4-8_ZMB_15k-points_1000-features_yr-2016-2022_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
+    # f1 = "landsat-c2-l2_bands-r-g-b-nir-swir16-swir22_ZMB_20k-points_1024-features_yr-2009-2021_mn-4-9_lm-True_cm-True_wa-False_summary.feather"
+    # f2 = "sentinel-2-l2a_bands-2-3-4-8_ZMB_15k-points_1000-features_yr-2016-2022_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
 
-    # f1 = "landsat-8-c2-l2_bands-1-2-3-4-5-6-7_ZMB_15k-points_1000-features_yr-2014-2021_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
-    # f2 = "sentinel-2-l2a_bands-2-3-4_ZMB_4k-points_1000-features_yr-2016-2022_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
+    f1 = "landsat-8-c2-l2_bands-1-2-3-4-5-6-7_ZMB_15k-points_1000-features_yr-2014-2021_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
+    f2 = "sentinel-2-l2a_bands-2-3-4_ZMB_4k-points_1000-features_yr-2016-2022_mn-1-12_lm-False_cm-True_wa-False_summary.feather"
 
     anom = True
 
@@ -23,12 +23,17 @@ if __name__ == "__main__":
 
     # inc_clim = False
     # clim = None
+    
+    if anom:
+        he = False
+    else:
+        he = True
 
     kwarg_list = [
         {
             "f1": f1,
             "f2": f2,
-            "he": True,
+            "he": he,
             "anomaly": anom,
             "split": split,
             "random_state": random_state,
@@ -52,11 +57,11 @@ if __name__ == "__main__":
     today = date.today().strftime("%Y-%m-%d")
 
     results = pd.DataFrame(output)
-    results_fn = f'2_sensor_top-mod_{n_splits}-splits_{today}_rcf_climate-{inc_clim}_anom-{anom}.csv'
+    results_fn = f'2_sensor_top-ot-mod_{n_splits}-splits_{today}_rcf_climate-{inc_clim}_anom-{anom}.csv'
     print(f"Saving results as: {results_fn}\n\n")
     results.to_csv(here("data","results", results_fn), index=False)
 
     oos_predictions = pd.concat(oos_preds)
-    oos_fn = f'2_sensor_top-mod_oos_predictions_{n_splits}-splits_{today}_rcf_climate-{inc_clim}_anom-{anom}.csv'
+    oos_fn = f'2_sensor_top-ot-mod_oos_predictions_{n_splits}-splits_{today}_rcf_climate-{inc_clim}_anom-{anom}.csv'
     print(f"Saving results as: {oos_fn}\n\n")
     oos_predictions.to_csv(here("data","results", oos_fn), index=False)
